29dc79e3202093782f13ca0bd070a82e151eeaf1
bifnhlp <bifnhlp@gmail.com>
2016-05-22 13:33:17 -0700

    Update core.py
    modify import

--- core.py
+++ core.py
@@ -24,6 +24,7 @@ from algorithms.exp3.exp3 import *
 from algorithms.hedge.hedge import *
 from algorithms.racing.racing import *
 from algorithms.bast.bast import *
+from algorithms.LUCB.LUCB import *
 
 # Testing framework
 from testing_framework.tests import *
